756efe565d2a36eaf3265c04779d61749d5fc4de
Eric Bailey <git@esb.lol>
2023-07-05 16:16:23 -0500

    chore: update release and commit setup

---
+++ release.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  branches: [
+    "main",
+    { name: "beta", prerelease: true },
+    { name: "alpha", prerelease: true },
+  ],
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/git",
+  ],
+};
